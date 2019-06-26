344839a4e8902ee9df5dc615ed89608a73ed8c67
daanwerf <daanvanderwerf@hotmail.com>
2019-06-26 14:40:42 +0200

    remove failed ML model and edited load_data method

--- Traditional/semantic_classifier.py
+++ Traditional/semantic_classifier.py
@@ -6,11 +6,6 @@ It gets to 75% validation accuracy in 25 epochs, and 79% after 50 epochs.
 '''
 
 from __future__ import print_function
-import keras
-from keras.datasets import cifar10
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Conv2D, MaxPooling2D
 import numpy as np
 import os
 
@@ -21,10 +16,10 @@ def load_data():
         Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
     """
     # 8000 training samples, 2000 test samples
-    x_train = np.empty((8000, 768, 1, 1), dtype='uint8')
+    x_train = np.empty((8000, 768, 1), dtype='uint8')
     y_train = np.empty((8000,), dtype='uint8')
 
-    x_test = np.empty((2000, 768, 1, 1), dtype='uint8')
+    x_test = np.empty((2000, 768, 1), dtype='uint8')
     y_test = np.empty((2000,), dtype='uint8')
 
     count = 1
@@ -35,16 +30,13 @@ def load_data():
         for j in range(100):
             filename = str(i) + "_" + str(count)
             p = np.load('featuredb/' + filename + ".npy")
-            feat_vec = np.empty((768, 1, 1), dtype='uint8')
-            for k in range(len(p)):
-                feat_vec[k] = np.reshape(p[k], (1, 1))
 
             if j < 80:
-                x_train[train_count] = feat_vec
+                x_train[train_count] = p
                 y_train[train_count] = i
                 train_count = train_count + 1
             else:
-                x_test[test_count] = feat_vec
+                x_test[test_count] = p
                 y_test[test_count] = i
                 test_count = test_count + 1
 
@@ -52,74 +44,4 @@ def load_data():
 
     return (x_train, y_train), (x_test, y_test)
 
-batch_size = 32
-num_classes = 100
-epochs = 50
-num_predictions = 20
-save_dir = os.path.join(os.getcwd(), 'saved_models')
-model_name = 'keras_trained_model.h5'
-
-# The data, split between train and test sets:
-(x_train, y_train), (x_test, y_test) = load_data()
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
-
-# Convert class vectors to binary class matrices.
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
-
-model = Sequential()
-model.add(Conv2D(768, 1, padding='same',
-                 input_shape=x_train.shape[1:]))
-model.add(Activation('relu'))
-model.add(Conv2D(768, 1))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Conv2D(1536, 1, padding='same'))
-model.add(Activation('relu'))
-model.add(Conv2D(1536, 1))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Flatten())
-model.add(Dense(512))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes))
-model.add(Activation('softmax'))
-
-# initiate RMSprop optimizer
-opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
-
-# Let's train the model using RMSprop
-model.compile(loss='categorical_crossentropy',
-              optimizer=opt,
-              metrics=['accuracy'])
-
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-
-print('Not using data augmentation.')
-model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          validation_data=(x_test, y_test),
-          shuffle=True)
-
-# Save model and weights
-if not os.path.isdir(save_dir):
-    os.makedirs(save_dir)
-model_path = os.path.join(save_dir, model_name)
-model.save(model_path)
-print('Saved trained model at %s ' % model_path)
-
-# Score trained model.
-scores = model.evaluate(x_test, y_test, verbose=1)
-print('Test loss:', scores[0])
-print('Test accuracy:', scores[1])
+print(load_data())
