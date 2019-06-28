8d134cf5a44c252dadacfeef7e20e6a080482c42
BigSorry <lexmeulenkamp@hotmail.nl>
2019-06-28 20:55:49 +0200

    Random forest with precision and recall results.

--- Traditional/classify.py
+++ Traditional/classify.py
@@ -1,35 +1,68 @@
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn import svm
-from sklearn.model_selection import cross_validate
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import cross_validate, train_test_split
 import numpy as np
 import glob
 import re
+import pickle
 
-def getScore(classifier, features, labels):
+trainSplit = 0.9
+classes = 100
+samplesClass = 100
+def getValidationScore(classifier, features, labels):
     classifier.fit(features, labels)
     scoring = {'recall_macro', 'precision_macro'}
     return cross_validate(classifier, features, labels, cv=5, scoring=scoring)
 
-features = []
-labels = []
-count = 0
-samples = 100*15
-for file in glob.glob("featuredb/*.npy"):
-    if samples == count:
-        break
-    labels.append(re.findall('[0-9]+', file)[0])
-    features.append(np.load(file).T)
-    count += 1
+def getTestLabels(classifier, train, trainLabel, test):
+    classifier.fit(train, trainLabel)
+    return classifier.predict(test)
+
+def getScores(predicted, testLabels):
+    correctArray = [0 for i in range(classes)]
+    predictionsClass = [0 for i in range(classes)]
+    for i in range(len(testLabel)):
+        predictionsClass[int(predicted[i])] += 1
+        if predicted[i] == testLabels[i]:
+            correctArray[int(predicted[i])] += 1
+    return {"precision" : [correctArray[i] / predictionsClass[i] for i in correctArray],
+            "recall" : [correctArray[i] / 100*trainSplit for i in correctArray]}
+
+features = np.zeros((10000, 768))
+labels = np.zeros(10000)
+for index, file in enumerate(glob.glob("featuredb/*.npy")):
+    features[index] = np.load(file).T
+    labels[index] = int(re.findall('[0-9]+', file)[0])
+
+runs = 10
+scores = {"precision" : 0, "recall" : 0}
+for run in range(runs):
+    print("Run {}".format(run))
+    # Fair splitting can be done more efficient.
+    trainIndices = np.array([np.random.choice(np.arange(i*classes, i*classes+100), replace=False, size=int(100*trainSplit)) for i in range(classes)]).flatten()
+    testIndices = np.setdiff1d(np.arange(10000), trainIndices)
+    train = features[trainIndices]
+    trainLabel = labels[trainIndices]
+    test = features[testIndices]
+    testLabel = labels[testIndices]
+
+    models = [RandomForestClassifier(n_estimators=128, random_state=0)]
+    names = ["Random_Forest"]
+    scaler = StandardScaler().fit(features)
+
+    # TODO save best model
+    save = True
+    for index, model in enumerate(models):
+        predicted = getTestLabels(model,scaler.transform(train),trainLabel, scaler.transform(test))
+        if save: pickle.dump(model, open(names[index], 'wb'))
+        score = getScores(predicted, testLabel)
+        scores['precision'] += np.mean(score['precision'])
+        scores['recall'] += np.mean(score['recall'])
+
+print("Average precision is {} and average recall is {}".
+      format(scores['precision'] / runs, scores['recall'] / runs))
 
 
-features = np.array(features).reshape(samples, features[0].shape[1])
-# TODO more models
-# TODO grid search for hyperparameters svc
-models = [LogisticRegression(random_state=0, solver='lbfgs',multi_class='multinomial'), svm.SVC(gamma='scale', decision_function_shape='ovo'), KNeighborsClassifier(n_neighbors=3)]
-for model in models:
-    score = getScore(model,features,labels)
-    print("Precision is {}".format(score["test_precision_macro"]))
-    print("Recall is {} \n".format(score["test_recall_macro"]))
 
 
