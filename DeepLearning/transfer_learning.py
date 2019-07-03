1a3048d0c8ff4e362b3c875d31805fbace5d12a2
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-07-03 16:12:26 +0000

    dl models

--- DeepLearning/transfer_learning.py
+++ DeepLearning/transfer_learning.py
@@ -8,19 +8,52 @@ from PIL import Image
 import re
 import os
 import numpy as np
+import pickle
+
+def testModel(model, test):
+    errors = 0
+    total = 0
+    with torch.no_grad():
+        for data in test:
+            images, labels = data
+            if torch.cuda.is_available():
+                images = images.cuda()
+                labels = labels.cuda()
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            errors += (predicted != labels).sum().item()
+    print("error rate is {}".format(errors / total))
 
 imgSize = 244
-
+classes = 100
+featureDimension = 25088
+# Fair splitting can be done more efficient.
+trainSplit = 0.7
+trainIndices = np.array([np.random.choice(np.arange(i*classes, i*classes+100), replace=False, size=int(100*trainSplit)) for i in range(classes)]).flatten()
+testIndices = np.setdiff1d(np.arange(10000), trainIndices)
+# Remove the image index for failed image
+if 9999 in trainIndices:
+    trainIndices = np.delete(trainIndices, np.argwhere(trainIndices == 9999))
+else:
+    testIndices = np.delete(testIndices, np.argwhere(testIndices == 9999))
+    
 path = "db_features/*.npy"
 pattern = "[0-9]+"
-featureDimension = 25088
 train = []
+test = []
 for index, file in enumerate(glob.glob(path)):
     label = int(re.findall(pattern, file)[0])
-    train.append([np.load(file), label])
-    if index == 100:
-        break
-
+    if index in trainIndices:
+        train.append([np.load(file), label])
+    else:
+        test.append([np.load(file), label])       
+with open('validation.pkl', 'wb') as file:
+    pickle.dump(test, file)      
+# Already shuffled by random.choice
+trainloader = torch.utils.data.DataLoader(train, shuffle=False, batch_size=16)
+testloader = torch.utils.data.DataLoader(test, shuffle=False, batch_size=1)
+    
 classifier = nn.Sequential(
     nn.Linear(featureDimension, 4096),
     nn.ReLU(inplace=True),
@@ -31,14 +64,12 @@ classifier = nn.Sequential(
     nn.Linear(4096, 100)
 )
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(classifier.parameters(),lr=0.1,weight_decay=1e-4)
+optimizer = torch.optim.Adam(classifier.parameters(),lr=0.0001)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 classifier.to(device)
-trainloader = torch.utils.data.DataLoader(train, shuffle=True, batch_size=4)
-
-epochs = 2
+epochs = 10
 for epoch in range(epochs):
-    runningLoss = 0.0
+    runningLoss = 0.0    
     for i, data in enumerate(trainloader, 0):
         inputs, labels = data
         if torch.cuda.is_available():
@@ -53,7 +84,11 @@ for epoch in range(epochs):
         optimizer.step()
 
         runningLoss += loss.item()
-        if i % 100 == 0:  # print every 2000 mini-batches
+        if i % 500 == 0:  # print every 2000 mini-batches
             print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, runningLoss / 100))
+                  (epoch + 1, i + 1, runningLoss / 500))
             runningLoss = 0.0
+            
+    print("epoch {} done".format(epoch))
+    testModel(classifier, testloader)
+    torch.save(classifier.state_dict(), "model{}".format(epoch))
