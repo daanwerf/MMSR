00dffa98b95c7cb3c37c86e2684c952febdbe031
BigSorry <lexmeulenkamp@hotmail.nl>
2019-07-04 14:17:12 +0200

    changed demo

--- DeepLearning/demo.py
+++ DeepLearning/demo.py
@@ -23,40 +23,37 @@ for key, values in data.items():
         if row[1][1] == row[1][2]:
             correct += 1
     labelsInterest[label] += correct
-    recall[label] += correct / 100
 
-for key, val in recall.items():
-    print("Class is {} and recall is {}".format(key, val / 5))
-# for key,val in precision.items():
-#     print("Class is {} and recall is {}".format(recall))
+for key,val in precision.items():
+    print("Class is {} and recall is {}".format(recall))
 
-# pattern = "[0-9]+_[0-9]+"
-# imIndex = 0
-# pattern2 = "[0-9]+"
-# for key, value in data.items():
-#     label = int(re.findall(pattern2, key)[0])
-#     if label == 6:
-#         imageInfo = re.findall(pattern, key)[0]
-#         fig = plt.figure()
-#         with Image.open("{}{}.jpg".format(path,imageInfo)) as image:
-#             plt.imshow(image)
-#             plt.axis('off')
-#             plt.savefig("output/{}search.jpg".format(imIndex))
-#             plt.close(fig)
+pattern = "[0-9]+_[0-9]+"
+imIndex = 0
+pattern2 = "[0-9]+"
+for key, value in data.items():
+    label = int(re.findall(pattern2, key)[0])
+    if label == 6:
+        imageInfo = re.findall(pattern, key)[0]
+        fig = plt.figure()
+        with Image.open("{}{}.jpg".format(path,imageInfo)) as image:
+            plt.imshow(image)
+            plt.axis('off')
+            plt.savefig("output/{}search.jpg".format(imIndex))
+            plt.close(fig)
 
-#         fig = plt.figure()
-#         for index, item in enumerate(value):
-#             imageInfo = re.findall(pattern, item[1][0])[0]
-#             with Image.open("{}{}.jpg".format(path,imageInfo)) as image:
-#                 plt.subplot(5, 4,index+1)
-#                 plt.axis('off')
-#                 plt.text(0, 0, "{0:.4f}".format(item[0]))
-#                 plt.imshow(image)
-#         plt.savefig("output/{}top20.jpg".format(imIndex))
-#         plt.close(fig)
-#         imIndex+=1
-#         if imIndex == 5:
-#             break
+        fig = plt.figure()
+        for index, item in enumerate(value):
+            imageInfo = re.findall(pattern, item[1][0])[0]
+            with Image.open("{}{}.jpg".format(path,imageInfo)) as image:
+                plt.subplot(5, 4,index+1)
+                plt.axis('off')
+                plt.text(0, 0, "{0:.4f}".format(item[0]))
+                plt.imshow(image)
+        plt.savefig("output/{}top20.jpg".format(imIndex))
+        plt.close(fig)
+        imIndex+=1
+        if imIndex == 5:
+            break
 
 
 
