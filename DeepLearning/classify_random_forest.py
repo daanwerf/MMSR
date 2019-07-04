b97bf1065e4e5d568bf2a74977266e7cc8e8c9fb
daanwerf <daanvanderwerf@hotmail.com>
2019-07-04 10:41:05 +0200

    finalized code for traditional, deep and fusion demos

---
+++ DeepLearning/classify_random_forest.py
@@ -0,0 +1,74 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_validate
+import numpy as np
+import glob
+import re
+from sklearn.externals import joblib
+
+trainSplit = 0.7
+classes = 100
+samplesClass = 100
+def getValidationScore(classifier, features, labels):
+    classifier.fit(features, labels)
+    scoring = {'recall_macro', 'precision_macro'}
+    return cross_validate(classifier, features, labels, cv=5, scoring=scoring)
+
+def getTestLabels(classifier, train, trainLabel, test):
+    classifier.fit(train, trainLabel)
+    return classifier.predict(test)
+
+def getScores(predicted, testLabels):
+    correctArray = [0 for i in range(classes)]
+    predictionsClass = [0 for i in range(classes)]
+    for i in range(len(testLabels)):
+        predictionsClass[int(predicted[i])] += 1
+        if predicted[i] == testLabels[i]:
+            correctArray[int(predicted[i])] += 1
+    # Assume we have same amount of test objects for each class
+    positives = len(testLabels) / classes
+    return {"precision" : [correctArray[i] / predictionsClass[i] for i in range(len(correctArray))],
+            "recall" : [correctArray[i] / positives for i in range(len(correctArray))]}
+def loadModel(filename):
+    classifier = None
+    with open(filename, 'rb'):
+        classifier = joblib.load(filename)
+    return classifier
+
+features = np.zeros((10000, 25088))
+labels = np.zeros(10000)
+for index, file in enumerate(glob.glob("db_features/*.npy")):
+    features[index] = np.load(file).T
+    labels[index] = int(re.findall('[0-9]+', file)[0])
+
+runs = 10
+scores = {"precision" : 0, "recall" : 0}
+bestPerformance = 2**32
+for run in range(runs):
+    print("Run {}".format(run))
+    # Fair splitting can be done more efficient.
+    trainIndices = np.array([np.random.choice(np.arange(i*classes, i*classes+100), replace=False, size=int(100*trainSplit)) for i in range(classes)]).flatten()
+    testIndices = np.setdiff1d(np.arange(10000), trainIndices)
+    train = features[trainIndices]
+    trainLabel = labels[trainIndices]
+    test = features[testIndices]
+    testLabel = labels[testIndices]
+    # More estimator is generally better, but it becomes slower and some point it isnt worth it anymore to expand the estimators
+    models = [RandomForestClassifier(n_estimators=128, random_state=0)]
+    names = ["random_forest"]
+    for index, model in enumerate(models):
+        predicted = getTestLabels(model,train,trainLabel, test)
+        score = getScores(predicted, testLabel)
+        scores['precision'] += np.mean(score['precision'])
+        scores['recall'] += np.mean(score['recall'])
+        if bestPerformance > (np.mean(score['precision'])+ np.mean(score['recall'])):
+            print("Save best model")
+            joblib.dump(model, "{}.pkl".format(names[index]))
+            bestPerformance = (np.mean(score['precision']) + np.mean(score['recall']))
+
+print("Average precision is {} and average recall is {}".
+      format(scores['precision'] / runs, scores['recall'] / runs))
+
+
+
+
--- deep_learning_demo.py
+++ deep_learning_demo.py
@@ -11,43 +11,30 @@ warnings.filterwarnings("ignore")
 import re
 
 
-def calculate_difference(p1, feature_vector):
-    return np.sum(np.absolute(p1.get_feature_vector() - feature_vector)).astype(int)
+def calculate_difference(feature_vector1, feature_vector2):
+    return np.sum(np.absolute(feature_vector1 - feature_vector2)).astype(int)
 
 
 def get_best_images(classifier, image):
     dict = {}
 
-    i_classifier_category = int(np.asscalar(classifier.predict(image.get_feature_vector().transpose())))
+    i_feature_vector = image.get_deep_learning_feature_vector_from_db()
+    i_classifier_category = int(np.asscalar(classifier.predict(np.reshape(i_feature_vector, (-1, 1)).transpose())))
 
     for i in range(100):
-        print(str(i) + "%")
         for j in range(1, 101):
+            print(str(i) + "." + str(j) + "%")
             image2 = Image(i, j)
-            i2_feature_vector = image2.get_feature_vector()
+
+            if int(image2.get_image_id()) == 9145:
+                print("skipped broken image")
+                continue
+
+            i2_feature_vector = np.reshape(image2.get_deep_learning_feature_vector_from_db(), (-1, 1))
             i2_classifier_category = int(np.asscalar(classifier.predict(i2_feature_vector.transpose())))
 
             if i2_classifier_category == i_classifier_category:
-                distance = calculate_difference(image, i2_feature_vector)
-                dict[distance] = image2.get_file_name()
-
-    return collections.OrderedDict(sorted(dict.items()))
-
-def get_best_images_late_fusion(classifier1, classifier2, image):
-    dict = {}
-
-    i_classifier_category = int(np.asscalar(classifier1.predict(image.get_feature_vector().transpose())))
-
-    for i in range(100):
-        print(str(i) + "%")
-        for j in range(1, 101):
-            image2 = Image(i, j)
-            i2_feature_vector = image2.get_feature_vector()
-            i2_classifier1_category = int(np.asscalar(classifier1.predict(i2_feature_vector.transpose())))
-            i2_classifier2_category = int(np.asscalar(classifier2.predict(i2_feature_vector.transpose())))
-
-            if i2_classifier1_category == i2_classifier2_category == i_classifier_category:
-                distance = calculate_difference(image, i2_feature_vector)
+                distance = calculate_difference(i_feature_vector, i2_feature_vector)
                 dict[distance] = image2.get_file_name()
 
     return collections.OrderedDict(sorted(dict.items()))
@@ -57,8 +44,10 @@ def show_top_images(p):
     plt.axis('off')
     plt.imshow(cv2.cvtColor(p.get_image_rgb(), cv2.COLOR_BGR2RGB))
 
-    classifier = joblib.load('random_forest2.pkl')
+    classifier = joblib.load('random_forest_dl.pkl')
     dict = get_best_images(classifier, p)
+    print(len(dict))
+    print(dict)
 
     fig = plt.figure(figsize=(10, 10))
     columns = 4
@@ -74,30 +63,9 @@ def show_top_images(p):
 
     plt.show()
 
-def show_top_images_late_fusion(p):
-    plt.axis('off')
-    plt.imshow(cv2.cvtColor(p.get_image_rgb(), cv2.COLOR_BGR2RGB))
 
-    classifier1 = joblib.load('random_forest2.pkl')
-    classifier2 = joblib.load('random_forest2.pkl')
-    dict = get_best_images_late_fusion(classifier1, classifier2, p)
-
-    fig = plt.figure(figsize=(10, 10))
-    columns = 4
-    rows = 5
-
-    for i in range(1, columns * rows + 1):
-        path = r"Corel100" + "\\" + dict[list(dict)[i]] + ".jpg"
-        img = cv2.imread(path, 1)
-        fig.add_subplot(rows, columns, i)
-
-        plt.axis('off')
-        plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-
-    plt.show()
-
-def calculate_precision_and_recall_for_category(category = 1, first_x_images = 20, image_amount = 5):
-    classifier = joblib.load('random_forest2.pkl')
+def calculate_precision_and_recall_for_category(category = 11, first_x_images = 20, image_amount = 5):
+    classifier = joblib.load('random_forest_dl.pkl')
 
     retrieved_count = first_x_images*image_amount
     relevant_count = 0
@@ -124,8 +92,8 @@ def calculate_precision_and_recall_for_category(category = 1, first_x_images = 2
 
 # Uncomment this line to get a visual overview of the 20 most for an Image with the first argument being the
 # category (0 - 99) and the second argument the index of the picture (1 - 100)
-show_top_images(Image(9, 36))
-show_top_images_late_fusion(Image(9, 36))
+show_top_images(Image(2, 64))
+
 
 # Uncomment this line to find the precision and recall of this method for a certain category
 # (default: category = 1 (Bears), first_x_images = 20, image_amount = 5)
